390d67f119cbf21fae3a442a473b99b2aa0ca51f
TatSu <tatsuya0316777@outlook.jp>
2022-05-23 18:17:01 +0900

    連結テスト
    試しに連結のコードを書きました。

---
+++ edit.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+im1 = cv2.imread('image/asia.jpg')
+im2 = cv2.imread('image/italy.jpg')
+
+im_v = cv2.vconcat([im1, im1])
+cv2.imwrite('asia^2.jpg', im_v)
+
+def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
+    w_min = min(im.shape[1] for im in im_list)
+    im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
+                      for im in im_list]
+    return cv2.vconcat(im_list_resize)
+im_v_resize = vconcat_resize_min([im1, im2, im1])
+cv2.imwrite('italyasia.jpg', im_v_resize)
