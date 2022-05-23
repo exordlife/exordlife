7dd23534b021d3b84015f4ab97f761b707a6a13c
TatSu <tatsuya0316777@outlook.jp>
2022-05-23 18:55:52 +0900

    編集

--- edit.py
+++ edit.py
@@ -5,12 +5,12 @@ im1 = cv2.imread('image/asi.jpg')
 im2 = cv2.imread('image/ita.jpg')
 
 im_v = cv2.vconcat([im1, im1])
-cv2.imwrite('asia^2.jpg', im_v)
+cv2.imwrite('maked-image/asia^2.jpg', im_v)
 
-def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
+def vconcat_resize(im_list, interpolation=cv2.INTER_CUBIC):
     w_min = min(im.shape[1] for im in im_list)
     im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
                       for im in im_list]
     return cv2.vconcat(im_list_resize)
-im_v_resize = vconcat_resize_min([im1, im2, im1])
-cv2.imwrite('itasia.jpg', im_v_resize)
+im_v_resize = vconcat_resize([im1, im2, im1])
+cv2.imwrite('maked-image/itasia.jpg', im_v_resize)
